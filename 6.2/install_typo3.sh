afb4d1ae2d7799e48095fab29cb5fb0184788fb6
eike.starkmann <eike.starkmann@undkonsorten.com>
2017-04-12 18:43:28 +0200

    REFACTOR: make image updatable, with source accesss (needed for xdebug)

--- 6.2/install_typo3.sh
+++ 6.2/install_typo3.sh
@@ -1,17 +1,13 @@
+rsync -av /typo3/typo3_src-$1*/ /var/www/html/typo3_src
 
-if [ ! -d /var/www/html/typo3/sysext ]
+if [ ! -d /var/www/html/typo3 ]
     then
-	
 	cd /var/www/html && \
-	wget -O - https://get.typo3.org/$1 | tar -xzf - && \
-    ln -s typo3_src-* typo3_src && \
-    ln -s  typo3_src/index.php index.php && \
-    ln -s  typo3_src/typo3 && \
+    ln -s typo3_src/index.php && \
+    ln -s typo3_src/typo3 && \
     ln -s typo3_src/_.htaccess .htaccess && \
     touch FIRST_INSTALL && \
-    touch typo3conf/ENABLE_INSTALL_TOOL && \
     chown -R www-data /var/www/html
-    
 fi
 
 apache2-foreground
